98043718e10c3c734a90acb0b64840888fc97d80
Junior Nunes <denisonprobable13@gmail.com>
2021-04-06 18:41:07 -0300

    BottomTabNavigation

--- src/Views/TelaC.js
+++ src/Views/TelaC.js
@@ -2,12 +2,14 @@ import React from 'react';
 import TextoCentral from '../Components/TextoCentral';
 
 export default props => {
-    const route = props.route || {params: { numero: 0 }};
+    const R = props.route;
+    const numero = R && R.params && R.params.numero
+        ? R.params.numero : 0
     return (
         <TextoCentral
             corFundo="#9932CD"
         >
-            Tela C - {props.route.params.numero}
+            Tela C - {numero}
         </TextoCentral>
     )
 }
---
+++ src/navegacao/Tab.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TelaA from '../Views/TelaA';
+import TelaB from '../Views/TelaB';
+import TelaC from '../Views/TelaC';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+export default props => {  
+    return (
+    <Tab.Navigator
+        initialRouteName="TelaB" 
+        tabBarOptions={{
+            activeTintColor: 'red',
+            inactiveTintColor: 'blue',
+            labelStyle: { fontSize: 30 }
+        }}
+    >
+      <Tab.Screen name="TelaA" component={TelaA} />
+      <Tab.Screen name="TelaB" component={TelaB} />
+      <Tab.Screen name="TelaC" component={TelaC} />
+    </Tab.Navigator>
+  );
+}
+
