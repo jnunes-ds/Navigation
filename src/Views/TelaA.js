f2aeb565108a3cb5ae0f2ea550b8b248755452da
Junior Nunes <denisonprobable13@gmail.com>
2021-04-05 23:49:49 -0300

    PassoStack, primeira forma

--- src/Views/TelaA.js
+++ src/Views/TelaA.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TextoCentral from '../Components/TextoCentral';
+import PassoStack from '../Components/PassoStack';
 
 export default props => {
     return (
-    <TextoCentral
-        corFundo="#E53935"
-    >
-        Tela A
-    </TextoCentral>
+        <PassoStack {...props} avancar="TelaB">
+            <TextoCentral
+                corFundo="#E53935"
+                >
+                Tela A
+            </TextoCentral>
+        </PassoStack>
     )
 }
